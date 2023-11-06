2690ef09cc0f4ccd354a3911b27e2b910a93e3f7
SovietMeteor10 <104686358+SovietMeteor10@users.noreply.github.com>
2023-11-06 20:24:17 +0000

    Update Home.js: trialling longer clear row duration

--- src/Home.js
+++ src/Home.js
@@ -163,7 +163,7 @@ function App2() {
         setRowToClear(prevRow => prevRow + 1);
 
         if (rowToClear < grid.length) {
-          setTimeout(clearRow, 100);
+          setTimeout(clearRow, 3000);
         }
       };
 
