d217778b6853b03efc35cccf8cddbbad81998bbf
SovietMeteor10 <anthonyduncalf@live.com>
2023-11-06 19:50:34 +0000

    timing change on row clearing

--- src/Home.js
+++ src/Home.js
@@ -163,7 +163,7 @@ function App2() {
         setRowToClear(prevRow => prevRow + 1);
 
         if (rowToClear < grid.length) {
-          setTimeout(clearRow, 800);
+          setTimeout(clearRow, 400);
         }
       };
 
